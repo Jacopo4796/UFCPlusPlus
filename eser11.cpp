7a22e418aaf8b45356add78216791470d1d17b82
Jacopo <jcp.federico@gmail.com>
2023-02-13 14:09:07 +0100

    modifiche eser11

--- eser11.cpp
+++ eser11.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
    int totale = 0;
-   for(int i = 1; i <= 100; i++)
+   for (int i = 1; i <= 100; i++)
    {
-      totale = totale += i;
+      totale = totale + i;
    }
    cout << "la somma dei numeri interi  da 1 a 100 è " << totale << "\n";
    return 0;
-   
-
 }
