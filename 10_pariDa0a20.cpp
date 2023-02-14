cfea0143efcd345eccdb1c2f7a3e98b0a7d0fc8a
Jacopo <jcp.federico@gmail.com>
2023-02-14 09:26:44 +0100

    modifica ; esercizio 10

--- 10_pariDa0a20.cpp
+++ 10_pariDa0a20.cpp
@@ -6,7 +6,7 @@ int main()
     cout << "i numeri pari da 0 a 20 sono: \n";
     for (int i = 0; i <= 20; i += 2)
     {
-        cout << i << "\n"
+        cout << i << "\n";
     }
     return 0;
 }
